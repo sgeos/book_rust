be543db7c38a44b4e48b8873c910e7b828c4b145
Brendan A R Sechter <b-sechter@unext.jp>
2019-05-16 18:45:51 +0900

    Added verbose output option.

--- Chapter3/temperature/src/main.rs
+++ Chapter3/temperature/src/main.rs
@@ -5,6 +5,12 @@
 extern crate clap;
 use clap::{Arg, App};
 
+const CELSIUS: &str = "Celsius";
+const KELVIN: &str = "Kelvin";
+const FAHRENHEIT: &str = "Fahrenheit";
+const UNKNOWN_FORMAT: &str = "unknown format";
+static FORMAT_LIST: &'static [&str] = &[CELSIUS, KELVIN, FAHRENHEIT];
+
 fn main() {
   let matches = App::new("Temperature Converter")
     .version("1.0")
@@ -20,16 +26,21 @@ fn main() {
        .short("t")
        .long("to")
        .default_value("c"))
+    .arg(Arg::with_name("VERBOSE")
+       .help("Verbose output flag")
+       .short("v")
+       .long("verbose"))
     .arg(Arg::with_name("VALUES")
        .multiple(true)
        .last(true))
     .get_matches();
 
-  let from = first_lowercase_character(matches.value_of("FROM"), '_');
-  let to = first_lowercase_character(matches.value_of("TO"), '_');
-  let values = match  matches.values_of("VALUES").map(|vals| vals.collect::<Vec<_>>()) {
-    Some(v) => v,
-    None => Vec::new(),
+  let verbose = matches.occurrences_of("VERBOSE");
+  let from = get_format(matches.value_of("FROM"));
+  let to = get_format(matches.value_of("TO"));
+  let values = matches.values_of("VALUES").map(|vals| vals.collect::<Vec<_>>()).unwrap_or(Vec::new());
+  if 0 < verbose {
+    println!("Convering {} temperature values from {} to {}.", values.len(), from, to);
   };
   for value in values.iter() {
     let input = match value.trim().parse() {
@@ -43,30 +54,30 @@ fn main() {
   println!("");
 }
 
-fn first_lowercase_character(input: Option<&str>, default: char) -> char {
-  match input {
-    Some(s) => match s.to_lowercase().chars().next() {
-      Some(c) => c,
-      None => default,
-    },
-    None => default,
+fn get_format(input: Option<&str>) -> &str {
+  let input = input.unwrap_or(UNKNOWN_FORMAT).trim().to_lowercase();
+  for format in FORMAT_LIST {
+    if format.to_lowercase().starts_with(&input) {
+      return format
+    }
   }
+  return UNKNOWN_FORMAT
 }
 
-fn t_to_c(t_type: char, t: f64) -> f64 {
+fn t_to_c(t_type: &str, t: f64) -> f64 {
   match t_type {
-    'f' => (t - 32.0) * 5.0 / 9.0,
-    'k' => t - 273.15,
-    'c' => t,
+    CELSIUS => t,
+    KELVIN => t - 273.15,
+    FAHRENHEIT => (t - 32.0) * 5.0 / 9.0,
     _ => std::f64::NAN,
   }
 }
 
-fn c_to_t(t_type: char, t: f64) -> f64 {
+fn c_to_t(t_type: &str, t: f64) -> f64 {
   match t_type {
-    'f' => t *  9.0 / 5.0 + 32.0,
-    'k' => t + 273.15,
-    'c' => t,
+    CELSIUS => t,
+    KELVIN => t + 273.15,
+    FAHRENHEIT => t *  9.0 / 5.0 + 32.0,
     _ => std::f64::NAN,
   }
 }
