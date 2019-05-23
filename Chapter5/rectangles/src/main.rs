fbc4c73de5c3f84d9f7ed790c994a4bfe0cb563c
Brendan A R Sechter <b-sechter@unext.jp>
2019-05-23 11:50:04 +0900

    Added second method.

--- Chapter5/rectangles/src/main.rs
+++ Chapter5/rectangles/src/main.rs
@@ -4,15 +4,27 @@ struct Rectangle {
   height: u32,
 }
 
-fn main() {
-  let rect1 = Rectangle { width: 30, height: 50 };
+impl Rectangle {
+  fn area(&self) -> u32 {
+    self.width * self.height
+  }
 
-  println!("rect1 is {:?}", rect1);
-  println!("rect1 is {:#?}", rect1);
-  println!("The area of the rectangle is {} square pixels.", area(&rect1));
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    other.width < self.width && other.height < self.height
+  }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-  rectangle.width * rectangle.height
+fn main() {
+  let rect1 = Rectangle { width: 30, height: 50 };
+  let rect2 = Rectangle { width: 10, height: 40 };
+  let rect3 = Rectangle { width: 60, height: 45 };
+
+  println!("rect1 is {:#?}", rect1);
+  println!("rect2 is {:#?}", rect2);
+  println!("rect3 is {:#?}", rect3);
+
+  println!("The area of rect1 is {} square pixels.", rect1.area());
+  println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+  println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
