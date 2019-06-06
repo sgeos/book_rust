f2b861bb511011f4d47b1719063ff0cab74adf6f
Brendan A R Sechter <b-sechter@unext.jp>
2019-06-07 00:45:14 +0900

    Printing errors to stderr.

--- Chapter12/minigrep/src/main.rs
+++ Chapter12/minigrep/src/main.rs
@@ -8,16 +8,16 @@ use std::process;
 fn main() -> Result<(), Box<dyn Error>> {
   let args: Vec<String> = env::args().collect::<Vec<_>>();
   let config = Config::new(&args).unwrap_or_else(|error| {
-    println!("Application Error: {}", error);
+    eprintln!("Application Error: {}", error);
     let executable = match args.len() {
       0 => "minigrep",
       1 | _ => args[0].as_str(),
     };
-    println!("Usage: {} PATTERN FILENAME", executable);
+    eprintln!("Usage: {} PATTERN FILENAME", executable);
     process::exit(1);
   });;
   if let Err(error) = minigrep::run(config) {
-    println!("Application Error: {}", error);
+    eprintln!("Application Error: {}", error);
     process::exit(1);
   }
   Ok(())
