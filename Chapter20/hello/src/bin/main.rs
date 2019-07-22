779e9bea6790641d30a27f2a8f716fd3733f53d4
Brendan A R Sechter <b-sechter@unext.jp>
2019-07-22 23:44:52 +0900

    Completed section 20.2 on using a multi-thread architecture.

--- Chapter20/hello/src/bin/main.rs
+++ Chapter20/hello/src/bin/main.rs
@@ -1,7 +1,11 @@
+use hello::ThreadPool;
+
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
   let variation = 0;
@@ -13,7 +17,8 @@ fn main() {
     4 => wrap_function("Main D".to_string(), main_d),
     5 => wrap_function("Main E".to_string(), main_e),
     6 => wrap_function("Main F".to_string(), main_f),
-    _ => main_f(),
+    7 => wrap_function("Main G".to_string(), main_g),
+    _ => main_g(),
   };
 }
 
@@ -142,7 +147,48 @@ fn handle_connection_f(mut stream: TcpStream) {
   println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
   let get = b"GET / HTTP/1.1\r\n";
+  let sleep = b"GET /sleep HTTP/1.1\r\n";
   let (status, headers, filename) = if buffer.starts_with(get) {
+    ( "HTTP/1.1 200 OK", "", "hello.html", )
+  } else if buffer.starts_with(sleep) {
+    thread::sleep(Duration::from_secs(5));
+    ( "HTTP/1.1 200 OK", "", "hello.html", )
+  } else {
+    ( "HTTP/1.1 404 NOT FOUND", "", "404.html", )
+  };
+
+  let body = fs::read_to_string(filename).unwrap();
+  let response = format!("{}\r\n{}\r\n{}", status, headers, body);
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+}
+
+fn main_g() {
+  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  let pool = ThreadPool::new(4);
+
+  for stream in listener.incoming() {
+    let stream = stream.unwrap();
+
+    pool.execute(
+      || {
+        handle_connection_g(stream);
+      }
+   );
+  }
+}
+
+fn handle_connection_g(mut stream: TcpStream) {
+  let mut buffer = [0; 512];
+  stream.read(&mut buffer).unwrap();
+  println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+  let get = b"GET / HTTP/1.1\r\n";
+  let sleep = b"GET /sleep HTTP/1.1\r\n";
+  let (status, headers, filename) = if buffer.starts_with(get) {
+    ( "HTTP/1.1 200 OK", "", "hello.html", )
+  } else if buffer.starts_with(sleep) {
+    thread::sleep(Duration::from_secs(5));
     ( "HTTP/1.1 200 OK", "", "hello.html", )
   } else {
     ( "HTTP/1.1 404 NOT FOUND", "", "404.html", )
