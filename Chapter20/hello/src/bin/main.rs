03aa52f9a3b5c02e7e2078ebfd61058b5f3c7f36
Brendan A R Sechter <b-sechter@unext.jp>
2019-07-24 22:37:53 +0900

    Completed section 20.3 on graceful shutdown and cleanup.

--- Chapter20/hello/src/bin/main.rs
+++ Chapter20/hello/src/bin/main.rs
@@ -18,7 +18,8 @@ fn main() {
     5 => wrap_function("Main E".to_string(), main_e),
     6 => wrap_function("Main F".to_string(), main_f),
     7 => wrap_function("Main G".to_string(), main_g),
-    _ => main_g(),
+    8 => wrap_function("Main H".to_string(), main_h),
+    _ => main_h(),
   };
 }
 
@@ -179,6 +180,46 @@ fn main_g() {
 }
 
 fn handle_connection_g(mut stream: TcpStream) {
+  let mut buffer = [0; 512];
+  stream.read(&mut buffer).unwrap();
+  println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+  let get = b"GET / HTTP/1.1\r\n";
+  let sleep = b"GET /sleep HTTP/1.1\r\n";
+  let (status, headers, filename) = if buffer.starts_with(get) {
+    ( "HTTP/1.1 200 OK", "", "hello.html", )
+  } else if buffer.starts_with(sleep) {
+    thread::sleep(Duration::from_secs(5));
+    ( "HTTP/1.1 200 OK", "", "hello.html", )
+  } else {
+    ( "HTTP/1.1 404 NOT FOUND", "", "404.html", )
+  };
+
+  let body = fs::read_to_string(filename).unwrap();
+  let response = format!("{}\r\n{}\r\n{}", status, headers, body);
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+}
+
+
+fn main_h() {
+  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  let pool = ThreadPool::new(4);
+
+  for stream in listener.incoming().take(2) {
+    let stream = stream.unwrap();
+
+    pool.execute(
+      || {
+        handle_connection_h(stream);
+      }
+   );
+  }
+
+  println!("Shutting down!");
+}
+
+fn handle_connection_h(mut stream: TcpStream) {
   let mut buffer = [0; 512];
   stream.read(&mut buffer).unwrap();
   println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
